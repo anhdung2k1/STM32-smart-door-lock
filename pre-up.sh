24dc51cd3c80f9eef3b132c0052bd2706cc36f14
anhdung2k1 <19521167@gm.uit.edu.vn>
2023-09-16 06:40:19 +0000

    code stm32

--- pre-up.sh
+++
@@ -1,6 +0,0 @@
-export root=$(git rev-parse --show-toplevel)
-echo $root
-export mysqlIp=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql_container)
-echo $mysqlIp
-docker-compose build authentication && docker-compose up authentication
-exec $SHELL -i #This will attach to the env
--- server/authentication/src/main/java/com/example/authentication/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.authentication.repository;
-
-import com.example.authentication.entity.UsersEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
-}
